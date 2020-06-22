267fcf6392e7866d032ff3e64afae647b8617210
kobeaibiancheng <wyx160413@yeah.net>
2020-06-22 21:35:12 +0800

    spring mvc

--- ee-study/src/main/java/yuxiang/controller/LoginController.java
+++ ee-study/src/main/java/yuxiang/controller/LoginController.java
@@ -1,9 +1,17 @@
 package yuxiang.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import yuxiang.model.User;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 @Controller
+@RequestMapping("/1")
 public class LoginController {
 
     /**
@@ -16,8 +24,60 @@ public class LoginController {
      *             2./login  --> 通过LoginController 中的方法 (转发)
      * @return
      */
-    @RequestMapping("/login")
+    @RequestMapping(value = "/login")
     public String login(){
         return "/login.html";
+    }
+
+    @RequestMapping(value = "/login2", method = RequestMethod.POST)
+    @ResponseBody
+    public Object login2(@RequestParam("username") String username1,
+                         String password,
+                         String password2){
+        System.out.println(username1+"="+password+", "+password2);
+        User user = new User();
+        user.setUsername("烤鸭");
+        user.setPassword("123");
+        user.setBirthday(new Date());
+        return user;
+    }
+
+    @RequestMapping(value = "/login3", method = RequestMethod.POST)
+    @ResponseBody
+    public Object login3(User u){
+        System.out.println(u);
+        User user = new User();
+        user.setUsername("烤鸭");
+        user.setPassword("123");
+        user.setBirthday(new Date());
+        return user;
+    }
+
+    @RequestMapping(value = "/login4",method = RequestMethod.POST)
+    @ResponseBody
+    public Object login4(@RequestBody User u) {
+        System.out.println(u);
+        User user = new User();
+        user.setUsername("北京");
+        user.setPassword("123");
+        user.setBirthday(new Date());
+        return user;
+    }
+
+    @RequestMapping(value = "/login5",method = RequestMethod.POST)
+    @ResponseBody
+    public Object login5(HttpServletRequest request,
+                         HttpServletResponse response,
+                         @RequestBody User u) {
+        if ("abc".equals(u.getUsername())&&"123".equals(u.getPassword())){
+            HttpSession session = request.getSession();
+            session.setAttribute("user",u);
+        }
+        System.out.println(u);
+        User user = new User();
+        user.setUsername("北京");
+        user.setPassword("123");
+        user.setBirthday(new Date());
+        return user;
     }
 }
--- ee-study/src/main/java/yuxiang/model/User.java
+++ ee-study/src/main/java/yuxiang/model/User.java
@@ -1,10 +1,16 @@
 package yuxiang.model;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+
+import java.util.Date;
 
 @Getter
 @ToString
-
+@Setter
 public class User {
+    private String username;
+    private String password;
+    private Date birthday;
 }
