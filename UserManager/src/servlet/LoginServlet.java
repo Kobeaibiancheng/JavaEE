d5a102e115f1f844f68ba19e3f2b0705cade6d18
kobeaibiancheng <wyx160413@yeah.net>
2020-07-19 21:09:45 +0800

    LoginServlet

--- UserManager/src/servlet/LoginServlet.java
+++ UserManager/src/servlet/LoginServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
+import dao.UserDao;
+import entiy.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
+import java.sql.SQLException;
 
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
@@ -15,5 +20,26 @@ public class LoginServlet extends HttpServlet {
         String password = req.getParameter("password");
         System.out.println("username " + username);
         System.out.println("password" + password);
+
+
+        Writer writer = resp.getWriter();
+        User user = new User();
+        user.setUserName(username);
+        user.setPassword(password);
+        System.out.println(user);
+        System.out.println("======================");
+        try {
+            user = UserDao.login(user);
+            System.out.println(user);
+            if (user != null) {
+                resp.sendRedirect("list.html");
+            }else {
+                System.out.println("failure");
+                writer.write("<h2> 登陆失败 </h2>");
+                resp.sendRedirect("login.html");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
