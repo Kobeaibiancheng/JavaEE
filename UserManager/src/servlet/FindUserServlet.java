f3560fddc7357907b9db44e56f7382ee40ab868c
kobeaibiancheng <wyx160413@yeah.net>
2020-07-21 22:24:15 +0800

    FindUserServlet

---
+++ UserManager/src/servlet/FindUserServlet.java
@@ -0,0 +1,38 @@
+package servlet;
+
+import entiy.User;
+import service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/findUserServlet")
+public class FindUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("application/json;charset=utf-8");
+
+
+        String idString = req.getParameter("id");
+        int id = Integer.parseInt(idString);
+        UserService userService = new UserService();
+        User user = null;
+        try {
+            user = userService.select(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if(user == null) {
+            System.out.println("没有要修改的对象！");
+        }else {
+            req.getSession().setAttribute("updateUser",user);
+            resp.sendRedirect("/update.html");
+        }
+    }
+}
