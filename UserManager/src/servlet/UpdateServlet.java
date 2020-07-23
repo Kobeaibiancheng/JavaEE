524916f8455aefae55a37daac8a97748383af650
kobeaibiancheng <wyx160413@yeah.net>
2020-07-23 23:09:49 +0800

    UpdateServlet

---
+++ UserManager/src/servlet/UpdateServlet.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import entiy.User;
+import service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/updateServlet")
+public class UpdateServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String gender = req.getParameter("gender");
+        String ageString = req.getParameter("age");
+        int age = Integer.parseInt(ageString);
+        String address = req.getParameter("address");
+        String qq = req.getParameter("qq");
+        String email = req.getParameter("email");
+
+        Object us = req.getSession().getAttribute("updateUser");
+        User user = (User)us;
+
+        User updateUser = new User();
+        updateUser.setId(user.getId());
+        updateUser.setName(name);
+        updateUser.setGender(gender);
+        updateUser.setAge(age);
+        updateUser.setAddress(address);
+        updateUser.setQq(qq);
+        updateUser.setEmail(email);
+
+        UserService userService = new UserService();
+        try {
+            int ret = userService.update(updateUser);
+            Map<String,Object> return_map = new HashMap<>();
+            if(ret == 1) {
+                return_map.put("msg",true);
+            }else {
+                return_map.put("msg",false);
+            }
+            //把登录成功的map返回给前端。json      : 便于前端进行处理。
+            ObjectMapper objectMapper = new ObjectMapper();
+            //就是将returnMap，转换为json字符串
+            objectMapper.writeValue(resp.getWriter(),return_map);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
