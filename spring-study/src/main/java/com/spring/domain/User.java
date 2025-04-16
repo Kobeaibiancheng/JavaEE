df1be572edc8cb19be265b957281c0e8d3e56521
Kobeaibiancheng <51166916+Kobeaibiancheng@users.noreply.github.com>
2025-04-16 15:02:12 +0800

    Create User.java

---
+++ spring-study/src/main/java/com/spring/domain/User.java
@@ -0,0 +1,30 @@
+package com.spring.domain;
+
+public class User {
+    private String name;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
