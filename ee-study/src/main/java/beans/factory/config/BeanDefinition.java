8f73534a76c358b932d7064842f2bcbe4934ed0a
Kobeaibiancheng <51166916+Kobeaibiancheng@users.noreply.github.com>
2025-06-30 23:00:06 +0800

    实现bean

---
+++ ee-study/src/main/java/beans/factory/config/BeanDefinition.java
@@ -0,0 +1,9 @@
+package beans.factory.config;
+
+public class BeanDefinition {
+    private Class beanClass;
+
+    public BeanDefinition(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+}
